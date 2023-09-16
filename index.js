d884c149d533608783091162843efa517fd0882e
festus-sulumeti <sulumetifestus@outlook.com>
2023-09-16 09:08:30 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,55 @@
+function add(a, b)
+{
+    let result;
+    result = a + b;
+    return result;
+}
+
+function subtract(a, b)
+{
+    let result;
+    result = a - b;
+    return result;
+}
+
+function multiply(a, b)
+{
+    let result;
+    result = a * b;
+    return result;
+}
+
+function divide(a, b)
+{
+    let result;
+    result = a / b;
+    return result;
+}
+
+function increment(n)
+{
+    let result;
+    result = ++n;
+    return result;
+}
+
+function decrement(n)
+{
+    let result;
+    result = --n;
+    return result;
+}
+
+function makeInt(n)
+{
+    let result;
+    result = parseInt(n, 10);
+    return result;
+}
+
+function preserveDecimal(n)
+{
+    let result;
+    result = parseFloat(n);
+    return result;
+}
